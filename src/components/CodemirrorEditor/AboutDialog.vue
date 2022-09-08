91d88ae00fe3bb3558277db9af975060fcef21ad
rogerwangzy <wangzhiyanroger@gmail.com>
2022-09-08 09:58:51 +0800

    Update AboutDialog.vue

--- src/components/CodemirrorEditor/AboutDialog.vue
+++ src/components/CodemirrorEditor/AboutDialog.vue
@@ -9,28 +9,7 @@
   >
     <div style="text-align: center">
       <h3>一款高度简洁的微信 Markdown 编辑器</h3>
-      <p>扫码关注公众号 Doocs，原创技术文章第一时间推送！</p>
-      <img
-        src="https://cdn-doocs.oss-cn-shenzhen.aliyuncs.com/gh/doocs/md/images/1648303220922-7e14aefa-816e-44c1-8604-ade709ca1c69.png"
-        style="width: 40%"
-      />
     </div>
-    <template slot="footer">
-      <el-button
-        type="primary"
-        @click="onRedirect('https://github.com/doocs/md')"
-        plain
-      >
-        GitHub 仓库
-      </el-button>
-      <el-button
-        type="primary"
-        @click="onRedirect('https://gitee.com/doocs/md')"
-        plain
-      >
-        Gitee 仓库
-      </el-button>
-    </template>
   </el-dialog>
 </template>
 
